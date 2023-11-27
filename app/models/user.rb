0fefe58860abca8377032e5e0015be7a7f0e4de2
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-11-27 12:01:44 +0000

    Add allowed_user_params to UsersController, write HTML form and then ERB form in new.html.erb, finding relevant commands in the API, NOT the Rails Guides

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :username, uniqueness: true
   validates :email, presence: true
-  valiates :email, uniqueness: true
+  validates :email, uniqueness: true
   validates :password, presence: true
 end
