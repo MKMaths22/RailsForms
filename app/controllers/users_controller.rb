20bc20078e6d349cc71f4eff9b0771d3f4ee7d76
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-11-28 15:33:41 +0000

    Add #edit and #update to UsersController and copy form to make edit.html.erb Add edit and update to routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+      if @user.update!(:username => params[:user][:username], :email => params[:user][:email], :password => params[:user][:password])
+        redirect_to new_user_path
+      else 
+        render :edit, status: :unprocessable_entity
+      end
+  end
+
   private
   
   def allowed_user_params
